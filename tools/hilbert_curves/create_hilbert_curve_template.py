f807d2eca2f8bd5393d3865700a05b177b49233f
zhgma <zhgma@outlook.com>
2025-02-20 16:32:24 +0800

    create efficient scan hilbert curve

--- tools/hilbert_curves/create_hilbert_curve_template.py
+++ tools/hilbert_curves/create_hilbert_curve_template.py
@@ -48,16 +48,25 @@ def hilbert_curve(order, orders, device):
     curve[s:, :s] = prev_curve.clone().detach().permute(1, 0).flip(0, 1) + 3 * s * s
     
     if order in orders:
-        hilbert_spatial_size = [2 * s, 2 * s]
         curve_flat = curve.view(-1)
         torch.save(curve_flat, f'./data/hilbert/hilbert_curve_rank_{order}.pth')
         print(f"张量已保存为 hilbert_curve_rank_{order}.pth")
+    if order+1 in orders:
+        s *= 2
+        curve_flat = torch.zeros(2 * s, 2 * s, device=device)
+        curve_flat[:s, :s] = curve.clone().detach()
+        curve_flat[:s, s:] = curve.clone().detach().permute(1, 0).flip(0) + s * s
+        curve_flat[s:, s:] = curve.clone().detach().flip(0, 1) + 2 * s * s
+        curve_flat[s:, :s] = curve.clone().detach().permute(1, 0).flip(1) + 3 * s * s
+        curve_flat = curve_flat.view(-1)
+        torch.save(curve_flat, f'./data/hilbert/hilbert_curve_es_rank_{order+1}.pth')
+        print(f"张量已保存为 hilbert_curve_es_rank_{order+1}.pth")
     return curve
 
 
 # 示例：生成3阶Hilbert曲线的张量并保存为pth文件
 if __name__ == "__main__":
-    orders = [7, 8, 9]  # 阶数
+    orders = [2, 3]  # 阶数
     hilbert_curve_to_tensor(orders)
 
     
